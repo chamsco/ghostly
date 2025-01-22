11345d10bd924111ec6e771e656c9449be1dc8f2
chamsco <cebagnitchie@gmail.com>
2025-01-23 00:17:23 +0100

    fixed the project creation flow again

--- backend/src/projects/projects.module.ts
+++ backend/src/projects/projects.module.ts
@@ -6,11 +6,12 @@ import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { Resource } from './entities/resource.entity';
 import { Environment } from './entities/environment.entity';
+import { EnvironmentVariable } from './entities/environment-variable.entity';
 import { EnvTemplateService } from './services/env-template.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Project, Resource, Environment]),
+    TypeOrmModule.forFeature([Project, Resource, Environment, EnvironmentVariable]),
     MulterModule.register({
       limits: {
         fileSize: 1024 * 1024 // 1MB
