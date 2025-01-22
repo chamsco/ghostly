c172e7c28d247a0c194a4845b910809801fefffa
chamsco <cebagnitchie@gmail.com>
2025-01-23 00:43:05 +0100

    added typeORm fixes

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,6 +1,10 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { Device } from '../auth/entities/device.entity';
+import { Project } from '../projects/entities/project.entity';
+import { Resource } from '../projects/entities/resource.entity';
+import { Environment } from '../projects/entities/environment.entity';
+import { EnvironmentVariable } from '../projects/entities/environment-variable.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -9,7 +13,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME || 'squadronuser',
   password: process.env.DB_PASSWORD || 'squadronpass',
   database: process.env.DB_DATABASE || 'squadron',
-  entities: [User, Device],
+  entities: [User, Device, Project, Resource, Environment, EnvironmentVariable],
   synchronize: true, // Temporarily enable for initial setup
   logging: true,
   dropSchema: false, // Don't drop existing tables
